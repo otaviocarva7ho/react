1bed57ca51ea9eadc9c761a01f7e266ac43cef4a
otaviocarva7ho <carvalhom7otavioh@gmail.com>
2023-12-05 20:17:38 -0300

    Trabalhando com props

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,25 +1,22 @@
 import './App.css';
-import HelloWorld from './components/HelloWorld';
+import SayMayName from './components/SayMayName';
+import Pessoa from './components/Pessoa';
 
 function App() {
 
-  const name = 'Carvalho'
-  const newName = name.toUpperCase()
-  const url = "https://via.placeholder.com/150"
-
-  function sum(a, b) {
-    return a + b
-  }
+  const nome = "Silva"
 
   return (
     <div className="App">
-      <h1>Olá React!</h1>
-      <p>Olá, {name}</p>
-      <p>{newName}</p>
-      <p>soma: {2 + 2}</p>
-      <p>soma: {sum(1, 2)}</p>
-      <img src={url} alt="Minha Imagem" />
-      <HelloWorld />
+      <SayMayName nome = "Carvalho" />
+      <SayMayName nome = "Isabelly" />
+      <SayMayName nome = {nome}/>
+      <Pessoa 
+        nome = "Otávio" 
+        idade = "18" 
+        profissao = "Programador" 
+        foto = "https://via.placeholder.com/150" 
+      />
     </div>
   );
 }
