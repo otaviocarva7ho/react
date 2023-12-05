0c7697a2a9fcf720c3ed3c98c38208eefc358f99
otaviocarva7ho <carvalhom7otavioh@gmail.com>
2023-12-04 22:07:38 -0300

    Entendendo o JSX

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,10 +1,23 @@
 import './App.css';
 
 function App() {
+
+  const name = 'Carvalho'
+  const newName = name.toUpperCase()
+  const url = "https://via.placeholder.com/150"
+
+  function sum(a, b) {
+    return a + b
+  }
+
   return (
     <div className="App">
       <h1>Olá React!</h1>
-      <p>Meu primeiro app</p>
+      <p>Olá, {name}</p>
+      <p>{newName}</p>
+      <p>soma: {2 + 2}</p>
+      <p>soma: {sum(1, 2)}</p>
+      <img src={url} alt="Minha Imagem" />
     </div>
   );
 }
